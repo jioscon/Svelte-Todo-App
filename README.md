af6b4d29d64f388144f71382c9bca124b1c8b291
jeycons <damonjey@gmail.com>
2021-02-13 13:37:17 +0900

    add update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # svelte todo app
 
-DEMO : <a href="https://sveltet-todo-app.netlify.app/" target="blank" rel="nofollow">https://sveltet-todo-app.netlify.app/</a>
+DEMO : <https://sveltet-todo-app.netlify.app/>
 
 참고 자료 : <https://svelte.dev/repl/7eb8c1dd6cac414792b0edb53521ab49?version=3.20.1>
--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,64 +1,61 @@
 <script>
   let now = new Date();
-  const months = [
-    'January',
-    'Fabruary',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
-  let year = now.getFullYear();
-  let month = months[now.getMonth()];
-  let date = now.getDate();
-  let day = week[now.getDay()];
-  let today = `${month} ${date} ${day}, ${year}`;
+  let options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+  let today = now.toLocaleDateString('ko-KR', options)
 
-  let newItem = '';
+  let inputTodo = '';
+  let modifyTodo = '';
   let todoList = [
-    { text: '스벨트로 Todo app 만들기', status: true },
-    { text: 'Reading an english book', status: false },
-    { text: 'Meeting some friends', status: false },
-    { text: 'Go to exercise and...', status: false },
+    { text: '스벨트로 Todo app 만들기', status: true, isEdit: false },
+    { text: 'Reading an english book', status: false, isEdit: false },
+    { text: 'Meeting some friends', status: false, isEdit: false },
+    { text: 'Go to exercise and...', status: false, isEdit: false },
   ];
 
-  function addToList() {
-    if (!newItem.trim()) {
-      newItem = '';
+  function createTodo() {
+    if (!inputTodo.trim()) {
+      inputTodo = '';
       return;
     }
-    todoList = [...todoList, { text: newItem, status: false }];
-    newItem = '';
+    todoList = [...todoList, { text: inputTodo, status: false, isEdit: false }];
+    inputTodo = '';
   }
 
-  function deleteToList(index) {
+  function onEdit(index) {
+    todoList[index].isEdit = true;
+    modifyTodo = todoList[index].text;
+  }
+
+  function offEdit(index) {
+    todoList[index].isEdit = false;
+  }
+
+  function updateTodo(index) {
+    todoList[index].text = modifyTodo;
+    todoList[index].isEdit = false;
+  }
+
+  function deleteTodo(index) {
     todoList.splice(index, 1);
     todoList = todoList;
   }
 
   $: sumItems = todoList.length;
-  $: leftItems = todoList.filter((s) => !s.status).length;
+  $: itemsLeft = todoList.filter((s) => !s.status).length;
 </script>
 
 <main class="container">
   <div class="todo-add">
     <input
-      bind:value={newItem}
+      bind:value={inputTodo}
       on:keydown={(e) => {
-        e.key === 'Enter' && addToList();
+        e.key === 'Enter' && createTodo();
       }}
       type="text"
       placeholder="new todo item.."
       aria-label="new todo item"
     />
-    <button on:click={addToList} type="button" aria-label="Add todo"
+    <button on:click={createTodo} type="button" aria-label="Add todo"
       >Add Todo</button
     >
   </div>
@@ -70,29 +67,55 @@
 
   <div class="todo-items">
     {#each todoList as item, index}
-      <div class="todo-item">
-        <label class:checked={item.status}>
-          <input bind:checked={item.status} type="checkbox" />
-          {item.text}
-        </label>
-        <button
-          on:click={() => deleteToList(index)}
-          type="button"
-          aria-label="Del">X</button
-        >
-      </div>
+      {#if item.isEdit}
+        <div class="todo-item">
+          <!-- svelte-ignore a11y-autofocus -->
+          <input
+            class="input-item"
+            bind:value={modifyTodo}
+            on:keydown={(e) => {
+              e.key === 'Enter' && updateTodo(index);
+            }}
+            type="text" autofocus />
+          <button
+            on:click={() => offEdit(index)}
+            type="button"
+            aria-label="Cancel">Cancel</button
+          >
+        </div>
       {:else}
+        <div class="todo-item">
+          <label class:checked={item.status}>
+            <input bind:checked={item.status} type="checkbox" />
+            {item.text}
+          </label>
+          <div>
+            <button
+              on:click={() => onEdit(index)}
+              type="button"
+              aria-label="Edit">Edit</button
+            >
+            <span>·</span>
+            <button
+              on:click={() => deleteTodo(index)}
+              type="button"
+              aria-label="Del">Del</button
+            >
+          </div>
+        </div>
+      {/if}
+    {:else}
       <div>Nothing...</div>
     {/each}
   </div>
   <div class="count">
     <span>Todo : {sumItems} </span>
-    <span>remaining : {leftItems}</span>
+    <span>{itemsLeft} {(itemsLeft > 1) ? 'items' : 'item'} left</span>
   </div>
 </main>
 
 <style>
   main {
-	min-height: 100vh;
-}
+    min-height: 100vh;
+  }
 </style>
